568cc3c79ce78fab3582ad0674b4dd4d0a47642d
Amar <onlyaboutcoc@gmail.com>
2025-10-29 12:39:20 +0530

    backedn url uploaded as per the deployement

--- frontend/app/api/api.ts
+++ frontend/app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Experience, BookingData, Booking } from '../../components/types';
 
-const API_BASE_URL = 'http://localhost:5500/api';
+const API_BASE_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
